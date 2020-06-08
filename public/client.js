d71046c7c5b8d45ca1e13c06e3af5d7312820553
NsanV <nsanv.dev@gmail.com>
2020-06-08 11:48:04 +0200

    video chat works

--- public/client.js
+++ public/client.js
@@ -6,12 +6,10 @@ const message = document.getElementById('message'),
     typing = document.getElementById('typing'),
     button = document.getElementById('button');
 
-    // send typing message
     message.addEventListener('keypress', () => {
         socket.emit('userTyping', handle.value)
     })
 
-    // send message to clients
     button.addEventListener('click', () => {
         socket.emit('userMessage', {
             handle: handle.value,
@@ -20,8 +18,6 @@ const message = document.getElementById('message'),
         document.getElementById('message').value = ''
     })
 
-
-    // listen from events from the server
     socket.on('userMessage', (data) => {
         typing.innerHTML = ''
         output.innerHTML += `<p> <strong>${data.handle}: </strong>${data.message}</p>`
@@ -30,3 +26,94 @@ const message = document.getElementById('message'),
     socket.on('userTyping', (data) => {
         typing.innerHTML = `<p><em>${data}: est en train d'écrire ...</em></p>`
     })
+
+    /* Video */
+
+    function getLocalVideo(callbacks) {
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+        const constraints = {
+            audio: false,
+            video: true
+        }
+        navigator.getUserMedia(constraints, callbacks.success, callbacks.error)
+    }
+
+    function receiveStream(stream, elemId) {
+        const video = document.getElementById(elemId)
+        video.srcObject = stream
+        window.peer_stream = stream
+    }
+
+    getLocalVideo({
+        success: (stream) => {
+            window.localstream = stream
+            receiveStream(stream, 'localVideo')
+        },
+        error: (err) =>{
+            alert(`Impossible d'accéder à votre caméra`)
+            console.log(err)
+        }
+    })
+
+    let conn;
+    let peer_id;
+
+    const peer = new Peer(); 
+
+    peer.on('open', (id) => {
+        document.getElementById('displayId').innerHTML = peer.id
+        // console.log('My peer ID is: ' + id)
+    })
+
+    peer.on('connection', (connection) => {
+        conn = connection
+        peer_id = connection.peer
+
+        document.getElementById('connectionId').value = peer_id
+    })
+    peer.on('error', (err) => {
+        alert('an error has happened: ' + err)
+        console.log(err)
+    })
+    document.getElementById('conn_button').addEventListener('click', () => {
+        peer_id = document.getElementById('connectionId').value
+
+        if (peer_id) {
+            conn = peer.connect(peer_id)
+        } else {
+            alert('enter an ID')
+            return false
+        }
+    })
+
+    peer.on("call", (call) => {
+        const acceptCall = confirm("Voulez-vous répondre à cet appel ?");
+        if (acceptCall) {
+            call.answer(window.localstream);
+            
+            call.on("stream", (stream) => {
+                window.peer_stream = stream;
+                receiveStream(stream, "remoteVideo")
+            });
+
+            call.on("close", () => {
+                alert("L'appel est terminé");
+            })
+        } else {
+            console.log("Appel rejeté")
+        }
+    });
+
+    document.getElementById("call_button").addEventListener("click", () => {
+        console.log("Appeler un contact: "+ peer_id);
+        console.log(peer);
+
+        let call = peer.call(peer_id, window.localstream)
+        call.on("stream", (stream) => {
+            window.peer_stream = stream;
+            receiveStream(stream, 'remoteVideo');
+        })
+    })
+
+
+
